65820b7f7a48bac0ba8a5930c9e0c27597266980
Ajay Makode <ajay.makode@thoughtworks.com>
2021-08-08 13:46:17 +0530

    Updating code

--- calculator.py
+++ calculator.py
@@ -1,5 +1,20 @@
+#This is addition programm
+
 def add(x, y):
   return x + y
+
+#Calculate subtraction
+
+
+
+
+#Calculate division
+
+
+
+
+#Calculate multiplication
+
 
 a = input("Enter first number: ")
 b = input("Enter second number: ")
